4f55daf4030b5909668ff1120934914ca6ad02a1
faresgin <44904357+faresgin@users.noreply.github.com>
2018-11-20 01:31:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,46 @@
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
-
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+const { Client, Util } = require('discord.js');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const queue = new Map();
+const UserBlocked = new Set();
+const prefix = '*'
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(Five Stars,"http://twitch.tv/S-F%22)
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(`Say : *help || *inv`, )
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log([Start] ${new Date()});
+  console.log(`[Start] ${new Date()}`);
   console.log('╚[═════════════════════════════════════════════════════════════════]╝')
   console.log('')
   console.log('╔[════════════════════════════════════]╗');
-  console.log(Logged in as * [ " ${client.user.username} " ]);
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
   console.log('')
   console.log('Informations :')
   console.log('')
-  console.log(servers! [ " ${client.guilds.size} " ]);
-  console.log(Users! [ " ${client.users.size} " ]);
-  console.log(channels! [ " ${client.channels.size} " ]);
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
   console.log('╚[════════════════════════════════════]╝')
   console.log('')
   console.log('╔[════════════]╗')
   console.log(' Bot Is Online')
+  console.log(' Bot with GMZN Host')
+  console.log(' Bot with GMZN Host')
+  console.log(' Bot with GMZN Host')
+  console.log(' Bot with GMZN Host')
+  console.log(' Bot with GMZN Host')
+  console.log(' Bot Is Online')
   console.log('╚[════════════]╝')
   console.log('')
   console.log('')
-});
+})
 
 client.login(process.env.BOT_TOKEN);
